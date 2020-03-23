fc02d25c4cb41bc7bf96aec98d4bccc8fabdb9ed
GuyPago <guypago@outlook.com>
2020-03-23 18:20:17 +0200

    Added Python file.

---
+++ Python project/ss.py
@@ -0,0 +1,11 @@
+greet = "Hello, "
+name = "Guy. "
+greetName = greet+name
+print(greetName*3)
+num = 25
+favoriteNumber = "Your favorite number is: "
+print(favoriteNumber+str(num))  # Turns variable to string. (str)
+print(len(favoriteNumber))  # prints the length of the string. (len)
+
+firstLetter = name[0]  # Gets the x letter of the string. [x]
+print("Your name's first letter is: " + firstLetter)
