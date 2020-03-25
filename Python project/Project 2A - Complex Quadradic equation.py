6e47da40b2f5c532d4f656fd129eb76f740b8beb
GuyPago <guypago@outlook.com>
2020-03-25 19:31:05 +0200

    Finished project 2A.

--- Python project/Project 2A - Complex Quadradic equation.py
+++ Python project/Project 2A - Complex Quadradic equation.py
@@ -1,16 +1,21 @@
-import math
+import cmath
 
 a = float(input("a: "))
 b = float(input("b: "))
 c = float(input("c: "))
 
-x1 = (-b + math.sqrt(b**2 - 4 * a * c)) / 2 * a
-x2 = (-b - math.sqrt(b**2 - 4 * a * c)) / 2 * a
+x1 = (-b + cmath.sqrt(b**2 - 4 * a * c)) / 2 * a
+x2 = (-b - cmath.sqrt(b**2 - 4 * a * c)) / 2 * a
 
-if x1 == int(x1):
-    x1 = int(x1)
-if x2 == int(x2):
-    x2 = int(x2)
+if x1 == x1.real:
+    x1 = x1.real
+    if x1 == int(x1):
+        x1 = int(x1)
+
+if x2 == x2.real:
+    x2 = x2.real
+    if x2 == int(x2):
+        x2 = int(x2)
 
 if x1 == x2:
     print("\nx = " + str(x1))
