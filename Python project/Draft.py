1483e3ca457595412fed2864cff86e66d9462b28
GuyPago <guypago@outlook.com>
2020-03-25 19:23:40 +0200

    Finished project 2 and added project 2A

--- Python project/Draft.py
+++ Python project/Draft.py
@@ -1,10 +1,18 @@
-import math
-math.sqrt(9)
+import cmath
+cmath.sqrt(4)
 a = float(input("a: "))
 b = float(input("b: "))
 c = float(input("c: "))
 
-x1 = (-b + math.sqrt(b**2 - 4*a*c))/2*a
-x2 = (-b - math.sqrt(b**2 - 4*a*c))/2*a
-print("x1 = " + str(x1))
-print("x2 = " + str(x2))
+x1 = (-b + cmath.sqrt(b**2 - 4*a*c))/2*a
+x2 = (-b - cmath.sqrt(b**2 - 4*a*c))/2*a
+print(x1.real)
+if x1 == x1.real:
+    x1 = int(x1.real)
+if x2 == int(x2):
+    x2 = int(x2)
+
+if x1 == x2:
+    print("\nx = " + str(x1))
+else:
+    print("\nAnswers are:\n" + "x1 = " + str(x1) + "\n" + "x2 = " + str(x2))
