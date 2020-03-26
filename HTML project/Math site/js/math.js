d5aea18a5edf7365575e127a5031b0afc0ea61fb
GuyPago <guypago@outlook.com>
2020-03-26 20:45:24 +0200

    fix

--- HTML project/Math site/js/math.js
+++ HTML project/Math site/js/math.js
@@ -3,5 +3,15 @@ var a = document.getElementById("aInput").value;
 var b = document.getElementById("bInput").value;
 var c = document.getElementById("cInput").value;
 
-document.getElementById("quadricOutput").innerHTML = "a";
+var x1 = (-b + Math.sqrt(Math.pow(b,2) - 4*a*c))/(2*a);
+var x2 = (-b - Math.sqrt(Math.pow(b,2) - 4*a*c))/(2*a);
+
+if (x1 === x2) {
+  document.getElementById("quadricOutput").innerHTML = "x = " + x1;
+
+}else {
+  document.getElementById("quadricOutput").innerHTML = "x1 = " + x1 + "<br> x2 = " + x2;
+
+}
+
 }
