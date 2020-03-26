c6985314619b2781b79333cbee262bf4109b5cbf
GuyPago <guypago@outlook.com>
2020-03-26 19:34:25 +0200

    update

---
+++ HTML project/Math site/js/math.js
@@ -0,0 +1,7 @@
+function solveQuadEquation() {
+var a = document.getElementById("aInput").value;
+var b = document.getElementById("bInput").value;
+var c = document.getElementById("cInput").value;
+
+document.getElementById("quadricOutput").innerHTML = "a";
+}
