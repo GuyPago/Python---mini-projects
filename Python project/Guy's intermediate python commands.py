6223c07bcea65d3f0fd8a6ca8e77fb46fa176ea9
GuyPago <guypago@outlook.com>
2020-04-12 22:57:54 +0300

    Added intermediate guide

--- Python project/Guy's intermediate python commands.py
+++ Python project/Guy's intermediate python commands.py
@@ -1,13 +1,12 @@
-import math
-math.sqrt(2)
 
-x = 0
-
+# Convert user input to 'ASCII' language.
 while True:
-    user_input = input("enter any key to print(enter 'q' to quit):\n",)
+    user_input = input("enter any key to print(enter 'q' to quit):\n") or "0"  # '0' is default input.
     if user_input == "q":
         print("ending program...bye!\n\n\n")
-        break
+        break  # Break/end the loop.
     else:
         print("user input was",user_input)
         print("the value of",user_input,"in ASCII is",str(ord(user_input)),"\n\n\n")
+
+# ^^^End^^^
