49e3ff554923ccb0379d4f1331858c07eb7a2d37
GuyPago <guypago@outlook.com>
2020-04-12 23:18:47 +0300

    Added content

--- Python project/Guy's intermediate python commands.py
+++ Python project/Guy's intermediate python commands.py
@@ -10,3 +10,15 @@ while True:
         print("the value of",user_input,"in ASCII is",str(ord(user_input)),"\n\n\n")
 
 # ^^^End^^^
+
+
+#  Multiplication table
+x = 10
+y = 10
+
+for i in range(1, y+1):   # set a range of numbers | range('start','end','jump')
+    for j in range(1, x+1):  # range always stops at (n-1). | range(0, n)
+        print(i * j, "\t",end="")  # set a 'tab' spacing. | '\t'
+    print()  # line break at the end of the loop.
+
+# ^^^End^^^
--- Python project/Math expirments.py
+++ Python project/Math expirments.py
@@ -1,13 +1,10 @@
 import math
 math.sqrt(2)
 
-x = 0
+x = 10
+y = 10
 
-while True:
-    user_input = input("enter any key to print(enter 'q' to quit):\n",)
-    if user_input == "q":
-        print("ending program...bye!\n\n\n")
-        break
-    else:
-        print("user input was",user_input)
-        print("the value of",user_input,"in ASCII is",str(ord(user_input)),"\n\n\n")
+for i in range(1,y + 1):
+    for j in range(1, x+1):
+        print(i * j, "\t",end="")
+    print()
