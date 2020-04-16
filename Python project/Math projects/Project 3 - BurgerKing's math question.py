8f92861fb47ecf239d1a6dec04a6fc3a24278bf2
GuyPago <guypago@outlook.com>
2020-04-16 13:53:10 +0300

    Finished project 3

--- Python project/Math projects/Project 3 - BurgerKing's math question.py
+++ Python project/Math projects/Project 3 - BurgerKing's math question.py
@@ -1,18 +1,7 @@
 import math
 
 # Burger-king's free woopher math question. Done by 2 different methods.
-
 def method_1():
-    n = 1
-    Sum = []
-    while (n <= 461):
-        solve = (-1)*((-1)**n * ((math.factorial(5)*5)/math.factorial(4))**(1/2))
-        Sum.append(solve)
-        n += 1
-    return sum(Sum)
-
-
-def method_2():
     n = 1
     Sum = 0
     while(n <= 461):
@@ -22,14 +11,14 @@ def method_2():
     return Sum
 
 
+def method_2():
+    n = 1
+    Sum = []
+    while (n <= 461):
+        solve = (-1)*((-1)**n * ((math.factorial(5)*5)/math.factorial(4))**(1/2))
+        Sum.append(solve)
+        n += 1
+    return sum(Sum)
 
-if method_1() == method_2():
-    print("Guy you were right")
-else:
-    print("WRONG!")
-method_2()
 
-gezza_kniyot = ["milk","shoko","batsek"]
-gezza_kniyot.append("mits")
-
-print(gezza_kniyot)
+print(method_1() == method_2())
