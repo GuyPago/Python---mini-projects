babd2d3fc6a607d7cd2c4513d47c347fb09aa461
GuyPago <guypago@outlook.com>
2020-05-07 23:27:41 +0300

    Improved project 5.

--- Python/Draft.py
+++ Python/Draft.py
@@ -1,5 +1,40 @@
 
 nums = [1,2,3,4,5,6,7,8,9,10]
 
-nums = list(filter(lambda x: x%2 == 0,nums))
 nums
+nums = list(filter(lambda x: x%2 == 0,nums))
+nums = list(map(lambda x: x**x,nums))
+nums = [i for i in nums if i%2 ==0]
+
+def is_prime(x):
+    flag = 1
+    if (x > 1):
+        for i in range(2,x):
+            if (x%i == 0):
+                return False
+            else:
+                flag = 0
+
+        if flag == 0:
+            return True
+    else:
+        return False
+is_prime(11)
+
+def all_primes():
+    n = 1000000
+    while True:
+        if is_prime(n):
+            yield n
+        n+=1
+all_primes()
+a = all_primes()
+next(a)
+for i in all_primes():
+    print(i)
+
+    עטינים = [i for i in range(1,9)]
+
+
+    for עטין in עטינים:
+        print(עטין)
--- Python/MathProjects/Project_5_Prime_checker.py
+++ Python/MathProjects/Project_5_Prime_checker.py
@@ -1,4 +1,4 @@
-
+# Check if number is prime:
 def prime_check(n):
     flag = 1
     for i in range(2,n):
@@ -9,10 +9,34 @@ def prime_check(n):
     if flag == 1:
         print(n,'is a prime number')
 
-
 prime_check(77655577)
 prime_check(1446111)
 
-24*60*60/5
-77655575/17280
-4494/365
+
+
+
+# Print all primes :
+
+def is_prime(x):
+    flag = 1
+    if (x > 1):
+        for i in range(2,x):
+            if (x%i == 0):
+                return False
+            else:
+                flag = 0
+
+        if flag == 0:
+            return True
+    else:
+        return False
+
+def all_primes():
+    n = 2
+    while True:
+        if is_prime(n):
+            yield n
+        n+=1
+
+for i in all_primes():
+    print(i)
