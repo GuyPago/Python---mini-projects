1c531d6e507a483b4fc0683a6351b38d6168283d
GuyPago <guypago@outlook.com>
2020-05-08 21:38:41 +0300

    Updated project 5

--- Python/MathProjects/Project_5_Prime_checker.py
+++ Python/MathProjects/Project_5_Prime_checker.py
@@ -8,35 +8,32 @@ def prime_check(n):
             break
     if flag == 1:
         print(n,'is a prime number')
-
-prime_check(77655577)
-prime_check(1446111)
-
-
-
-
-# Print all primes :
-
 def is_prime(x):
-    flag = 1
     if (x > 1):
         for i in range(2,x):
             if (x%i == 0):
                 return False
-            else:
-                flag = 0
+        return True
+#prime_check(77655577)
+#prime_check(1446111)
 
-        if flag == 0:
-            return True
-    else:
-        return False
 
+# Print specific primes range:
+
+def prime_range(start=1,end=10):
+    while (start < end):
+        if is_prime(start):
+            yield start
+        start+=1
+for i in prime_range(10000,11000):
+    print(i)
+
+# Print all primes :
 def all_primes():
-    n = 2
+    n = 1000000
     while True:
         if is_prime(n):
             yield n
         n+=1
-
 for i in all_primes():
     print(i)
