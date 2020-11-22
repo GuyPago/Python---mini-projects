b89192fc29cdebbf900b0df54437b5b304af5188
GuyPago <guypago@outlook.com>
2020-11-22 19:55:53 +0200

    Added Hw 1-4

---
+++ Python/HomeWork/Ex3/ex3_0123456789.py
@@ -0,0 +1,43 @@
+''' Exercise #3. Computational Thinking and Programming.'''
+
+#########################################
+# Question 1 - do not delete this comment
+#########################################
+lst = ??? # Replace ??? with a list of your choice.
+# Write the rest of the code for question 1 below here.
+
+
+
+#########################################
+# Question 2 - do not delete this comment
+#########################################
+lst = ??? # Replace ??? with a list of your choice.
+# Write the rest of the code for question 2 below here.
+
+
+
+#########################################
+# Question 3 - do not delete this comment
+#########################################
+lst = ??? # Replace ??? with a list of integer numbers of your choice.
+# Write the rest of the code for question 3 below here.
+
+
+
+#########################################
+# Question 4 - do not delete this comment
+#########################################
+x = ??? # Replace ??? with a value of your choice.
+lst = ??? # Replace ??? with a list of your choice.
+# Write the rest of the code for question 4 below here.
+
+
+
+#########################################
+# Question 5 - do not delete this comment
+#########################################
+lst = ??? # Replace ??? with a list of your choice.
+# Write the rest of the code for question 5 below here.
+
+
+
