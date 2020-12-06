57db9422c5d8bcc1ab26f61c992b6c48cf8ae05b
GuyPago <guypago@outlook.com>
2020-12-06 11:29:44 +0200

    Uploaded more HW

--- Python/HomeWork/Ex6/ex6_0123456789.py
+++ Python/HomeWork/Ex6/ex6_0123456789.py
@@ -1,48 +1,42 @@
-''' Exercise #5. Computational Thinking and Programming.'''
-import math as m
-
+''' Exercise #6. Computational Thinking and Programming.'''
 
 #########################################
 # Question 1 - do not delete this comment
 #########################################
-def is_char_in_str(s1, c1):
-    return c1 in s1
+def how_many_common_letters(s1, s2):
+    pass  # delete this pass command and write your code instead
+
 
 
 #########################################
 # Question 2 - do not delete this comment
 #########################################
-def is_in_range(lst2, a, b):
-    if len(lst2) > 0:
-        return (max(lst2) < b) and (min(lst2) > a)
-    else:
-        return True
+def clean_word(word, chars_list):
+    pass  # delete this pass command and write your code instead
+
 
 
 #########################################
 # Question 3 - do not delete this comment
 #########################################
-def upper_list_strings(lst3):
-    new_lst = []
-    for i in lst3:
-        new_lst.append(i.upper())
-    return new_lst
+def ciel_list_of_floats_in_place(floats_list):
+    pass  # delete this pass command and write your code instead
+
 
 
 #########################################
 # Question 4 - do not delete this comment
 #########################################
-def log10_list(lst4):
-    new_lst = []
-    for i in lst4:
-        new_lst.append(m.log10(i))
-    return new_lst
+def clean_words_of_sentence(sentence):
+    pass  # delete this pass command and write your code instead
+
 
 
 #########################################
 # Question 5 - do not delete this comment
 #########################################
-def is_palindrom(s5):
-    # return s5 == s5[::-1]
-    n=len(s5)
-    print(s5[])
+def is_valid_pattern(p):
+    pass  # delete this pass command and write your code instead
+    
+
+
--- Python/Math expirments.py
+++ Python/Math expirments.py
@@ -1,19 +1,7 @@
+s = 'Betanirs'
 
-import random
+first_half = s[:len(s)//2]
+second_half = s[len(s)//2:]
 
+print(first_half + second_half)
 
-
-
-
-random.gauss(89,6.04)
-
-x = [85,80,87]
-
-def ave(L):
-    return sum(L)/len(L)
-
-ave(x)
-sum = 0
-for i in x:
-    sum += i
-print(sum)
