4c330124257865dffe8fae3294cfda1fbc42c533
Guy Taggar <guypago@outlook.com>
2021-03-15 11:54:16 +0200

    Removed

--- SQL/Test.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE guy (
-  id INT NOT NULL,
-  name VARCHAR(20)
-);
-
-INSERT INTO guy
-VALUES(22, 'Betanir');
-SELECT id,name FROM guy
-WHERE id = 22;
--- test.r
+++
@@ -1,3 +0,0 @@
-library(openxlsx)
-x <- c("z", "b")
-read.csv("ps1.csv")
