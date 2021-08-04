ab78381abc293c59dea1cc4b0b1d7d307f560a8a
Guy Taggar <guypago@outlook.com>
2021-08-04 11:29:04 +0300

    Reordered files

--- Python/GeneralProjects/Project_3_SpeedTest.py
+++
@@ -1,9 +0,0 @@
-import speedtest as s
-
-check = s.Speedtest()
-
-download_speed = round(check.download()/(1000**2),2)
-upload_speed = round(check.upload()/(1000**2),2)
-
-print('''Results:\nDownload speed : {down} Mbps
-Upload speed : {up} Mbps'''.format(down=download_speed, up=upload_speed))
